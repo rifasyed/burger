4ce525bc7dad5f68ff14bdb3a51afd8c0cfb8dfd
Rifa Syed <rifarajafri@gmail.com>
2018-04-27 21:35:08 -0700

    added html

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var express = require('express')
 var bodyparser = require('body-parser')
+var path = require('path')
 
+// new express app
 var app = express()
 
-config.connect();
- 
-config.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
- 
-config.end();
+// middleware
+var expresshbs = require('express-handlebars')
+app.engine('hbs', expresshbs({defaultLayout: 'main'}))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+
+// var router = require('./controllers/burgers_controllers.js')
+// app.use('/', router)
+
+var PORT = process.env.PORT || 3000
+
+// listeningport
+app.listen(PORT, function (e) {
+  if (e) throw e
+  console.log("App listening on port: " + PORT)
+}) 
